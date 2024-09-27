aadf524c71fa8973cc7bcd66a078cbdf48a07113
estudanteandreceep <124880847+andreferreiradeandrade@users.noreply.github.com>
2024-09-27 09:34:26 -0300

    Update criapubli_back.php

--- projeto_FINAL/criapubli_back.php
+++ projeto_FINAL/criapubli_back.php
@@ -17,3 +17,40 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+
+<?php 
+
+if(isset($_POST['submit'])){
+    include_once('conexao.php');
+
+    $legenda= $_POST['legenda'];
+    
+
+    $result = mysqli_query($con, "INSERT INTO publicacoes(legenda) values ('$legenda')");
+
+    if($result){
+        header("Location: home.php");
+        exit();
+    }else{
+        echo "Erro ao publicar ". mysqli_error($con);
+    }
+}
+
+?>
+
+<?php
+session_start();
+
+include_once('conexao.php');
+
+if(! isset($_SESSION['id_usuario'])){
+    die('Você precisa estar logado para publicar');
+}
+
+if(isset($_POST['conteudo'])){
+    $conteudo = $_POST['conteudo'];
+    $id_usuario = $_SESSION['id_usuario'];
+    $sql = "INSERT INTO publicacoes"
+}
+
+?>
