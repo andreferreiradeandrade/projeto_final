f7dabec9674f83e78cd5acb3d85638a85235e97a
estudanteandreceep <124880847+andreferreiradeandrade@users.noreply.github.com>
2024-10-18 07:12:19 -0300

    Atualizar o criapubli_back.php

--- projeto_FINAL/criapubli_back.php
+++ projeto_FINAL/criapubli_back.php
@@ -1,23 +1,3 @@
-<?php 
-
-if(isset($_POST['submit'])){
-    include_once('conexao.php');
-
-    $legenda= $_POST['legenda'];
-    
-
-    $result = mysqli_query($con, "INSERT INTO publicacoes(legenda) values ('$legenda')");
-
-    if($result){
-        header("Location: home.php");
-        exit();
-    }else{
-        echo "Erro ao publicar ". mysqli_error($con);
-    }
-}
-
-?>
-
 <?php
 session_start();
 
